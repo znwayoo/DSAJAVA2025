ef47dcf17e5e1c7e1932a831961f348b13e26ba4
Zarni Nway Oo <znwayooo@gmail.com>
2025-02-03 15:49:49 +0700

    Commit directly from IDE to remote

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Commit Directly From IDE to Remote");
     }
 }
